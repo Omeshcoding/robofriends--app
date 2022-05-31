4df8566e8709708220a361138f52412b1802dc62
umesh <umesharma.us11@gmail.com>
2022-05-31 11:36:26 +0530

    robts object added

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const card = () => {
+const card = (props) => {
     return (
-        <div className='bg-emerald-500 w-52 m-8 p-10 rounded'>
-            <img src='https://robohash.org/test' alt='robots'></img>
-            <div>
-                <h2>John Doe</h2>
-                <p>john-doe@gmail.com</p>
+        <div className='bg-emerald-500 w-52 m-4 drop-shadow-md p-10 rounded inline-block  ease-in-out duration-300 hover:scale-105 '>
+            
+        <img src={`https://robohash.org/${props.id}?200 * 200`} alt='robots'></img>
+            <div className='text-center my-3'> 
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
