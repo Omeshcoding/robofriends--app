41967a9211bc397c480d127a0fc272f006f975d4
umesh <umesharma.us11@gmail.com>
2022-05-31 12:50:06 +0530

    added CardList

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const card = (props) => {
+    const {name, email}= props
     return (
-        <div className='bg-emerald-500 w-52 m-4 drop-shadow-md p-10 rounded inline-block  ease-in-out duration-300 hover:scale-105 '>
+        <div className='bg-emerald-500 w-52 m-4 drop-shadow-md  rounded inline-block text-center ease-in-out duration-300 hover:scale-105 '>
             
-        <img src={`https://robohash.org/${props.id}?200 * 200`} alt='robots'></img>
-            <div className='text-center my-3'> 
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+        <img src={`https://robohash.org/${props.id}`} className="p-8 pb-4" alt='robots'></img>
+            <div className=' my-3'> 
+                <h2 className=''>{name}</h2>
+                <p className=''>{email}</p>
             </div>
         </div>
     );
