4e965849af323e2ae165ca3c1caaee1eaf2b2248
umesh <umesharma.us11@gmail.com>
2022-06-03 20:47:50 +0530

    Api added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardList from './CardList';
+import Scroll from './Scroll';
 import {robots} from './robots';
 import Searchbox from './Searchbox';
 import './App.css';
@@ -11,10 +12,18 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
   }
-}
+  console.log('constructor')
+  }
+  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+      .then(users =>this.setState({robots:robots})       )
+    
+    console.log('check')
+  }
   
   onsearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -24,14 +33,22 @@ class App extends React.Component {
     const filteredRobots = this.state.robots.filter(robot=> {
       return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
     })
-    return (
-      <>
-        <h1 className='text-center text-7xl text-purple-900  my-4'>RoboFriends</h1>
-        <Searchbox searchChange={this.onsearchChange}/>
-        <CardList robots={filteredRobots} />
-      </>
-    );
-
+    if (this.state.robots.length === 0) {
+      return <h1>Loading...</h1>
+    } else {
+      
+      
+      return (
+        <>
+          <h1 className='text-center text-7xl text-purple-900  my-4'>RoboFriends</h1>
+          <Searchbox searchChange={this.onsearchChange} />
+         
+          <Scroll>
+          <CardList robots={filteredRobots} />
+          </Scroll>
+        </>
+      );
+    }
   }
 }
 export default App;
