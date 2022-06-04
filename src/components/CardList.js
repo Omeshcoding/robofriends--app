a0e1abc0481fa482115d0bfa1f3034dd9df0fc8c
umesh <umesharma.us11@gmail.com>
2022-06-04 12:25:56 +0530

    updating pakagejson

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,9 +3,7 @@ import Card from './Card';
 
 
 const CardList = ({ robots }) => {
-    if (true) {
-      throw new Error("Ohhhh")
-  }
+
     return (
  <div className='mx-20 text-center'>
             {
